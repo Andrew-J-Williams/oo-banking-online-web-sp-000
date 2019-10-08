6b90782adf8d8ccc66012e2414de0a1e43f51b14
Andrew Williams <1williamsaj@gmail.com>
2019-10-08 02:36:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,8 +9,8 @@ class Transfer
     @amount = amount
   end
 
-  def valid?
-    @sender.valid? && @receiver.valid? ? true : false
+  def valid? # Since our 'sender' and 'receiver' attributes are Class instances of 'BankAccount' class, we can use the 'valid?' method on them.
+    @sender.valid? && @receiver.valid? ? true : false # We compare to see if both are true.
   end
 
   def execute_transaction
