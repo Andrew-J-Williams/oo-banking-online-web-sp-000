7210238f988d17c1fd6425f0f2b33361b5b1cbd8
Andrew Williams <1williamsaj@gmail.com>
2019-10-08 03:08:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && self.status == 'pending'
+    if @sender.valid? && self.status == 'pending'
       @sender.balance -= self.amount
       @receiver.balance += self.amount
       self.status = 'complete'
