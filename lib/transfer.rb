5885511ea3a1dbc65a0242ceb45ea4fbcb48c654
Andrew Williams <1williamsaj@gmail.com>
2019-10-08 02:29:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,14 +10,7 @@ class Transfer
   end
 
   def valid?
-    account_one = BankAccount.new(sender)
-    account_two = BankAccount.new(receiver)
-
-    if account_one.valid?(true) && account_two.valid?(true)
-      true
-    else
-      false
-    end
+    
   end
 
 end
