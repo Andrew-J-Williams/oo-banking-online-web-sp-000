f941d33a342e7ac3f9b9ec64baced94b8268e867
Andrew Williams <1williamsaj@gmail.com>
2019-10-08 03:06:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,14 +14,14 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-     @sender.balance -= @amount
-     @receiver.balance += @amount
-     @status = "complete"
-   else
-     @status = "rejected"
-     return "Transaction rejected. Please check your account balance."
-   end
+    if @sender.balance > @amount && self.status == 'pending'
+      @sender.balance -= self.amount
+      @receiver.balance += self.amount
+      self.status = 'complete'
+    else
+      self.status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
   end
 
   def reverse_transfer
